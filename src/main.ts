4dd506c5edb14b3bcdb85537c3f401c4aab2ff46
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-07 10:12:35 +0700

    feat(application): initialize NestJS application

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { ExpressAdapter, NestExpressApplication } from "@nestjs/platform-express";
+
+import { AppModule } from "./app/app.module";
+
 class Bootstrap {
-  public static start(): void {
-    console.log("Application started successfully");
+  private static readonly logger: Logger = new Logger(Bootstrap.name);
+
+  public static async start(): Promise<void> {
+    try {
+      const application: NestExpressApplication = await NestFactory.create(AppModule, new ExpressAdapter());
+
+      await application.listen(3000);
+
+      this.logger.log(`Listening on port ${await application.getUrl()}`);
+      this.logger.log("Application started successfully");
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        this.logger.error(error.message, error.stack);
+      } else {
+        this.logger.error(error);
+      }
+
+      this.logger.error("Application failed to start");
+    }
   }
 }
 
-Bootstrap.start();
+void Bootstrap.start();
