f79fa1ba31cc2b50847f672649c7f8f5f57444d0
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-07 10:17:56 +0700

    chore(eslint): ignore dist directory in global

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ import yamlParser from "yaml-eslint-parser";
 const jsFiles = ["**/*.cjs", "**/*.js", "**/*.mjs"];
 const tsFiles = ["**/*.ts"];
 const configuration = eslint.defineConfig(
-  eslint.globalIgnores(["**/node_modules/*", "**/package-lock.json"]),
+  eslint.globalIgnores(["**/dist/*", "**/node_modules/*", "**/package-lock.json"]),
   {
     languageOptions: {
       globals: {
