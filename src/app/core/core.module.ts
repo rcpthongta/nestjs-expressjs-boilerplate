484909688b20ebc89ff6e9795cd265219329219f
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-09 12:36:09 +0700

    feat(core): integrate WinstonModule into CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
+import { winstonConfiguration } from "@configuration";
 import { environment, environmentSchema, EnvironmentSchema } from "@environment";
 
 import { Module, OnModuleInit } from "@nestjs/common";
 
 import * as Joi from "joi";
 
+import { WinstonModule } from "nest-winston";
+
 import { InvalidEnvironmentException } from "./exceptions";
 
-@Module({})
+@Module({
+  imports: [WinstonModule.forRoot({ ...winstonConfiguration })]
+})
 export class CoreModule implements OnModuleInit {
   private validEnvironment(): void {
     const { error }: Joi.ValidationResult<EnvironmentSchema> = environmentSchema.validate(environment);
