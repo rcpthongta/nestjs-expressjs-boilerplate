b12bc300953c9072be8d13a235e2de37c8cbc4f5
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-09 18:07:14 +0700

    refactor(logging): replace processId format with system format for enriched system metadata

--- src/configurations/winston/formats/index.ts
+++ src/configurations/winston/formats/index.ts
@@ -1,6 +1,6 @@
 export * from "./level-upper-case.format";
 export * from "./printf.format";
-export * from "./process-id.format";
 export * from "./request-id.format";
 export * from "./skip-message-by-contexts.format";
 export * from "./skip-message-by-levels.format";
+export * from "./system.format";
--- src/configurations/winston/formats/system.format.ts
+++ src/configurations/winston/formats/system.format.ts
@@ -1,12 +1,15 @@
+import os from "node:os";
 import process from "node:process";
 
 import * as winston from "winston";
 
 import { TransformableOptions } from "../interfaces";
 
-export const processId: () => winston.Logform.Format = (): winston.Logform.Format => {
+export const system: () => winston.Logform.Format = (): winston.Logform.Format => {
   return winston.format((transformable: TransformableOptions): TransformableOptions => {
+    transformable.hostname = os.hostname();
     transformable.pid = process.pid.toString();
+    transformable.node = process.version;
 
     return transformable;
   })();
