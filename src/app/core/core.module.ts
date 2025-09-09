4f758d8608f88227283da7b173317edf9eaab5ab
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-09 16:26:00 +0700

    feat(core): add MorganMiddleware to CoreModule and implement NestModule interface

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,24 +1,29 @@
 import { winstonConfiguration } from "@configuration";
 import { environment, environmentSchema, EnvironmentSchema } from "@environment";
 
-import { Module, OnModuleInit } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule, OnModuleInit, RequestMethod } from "@nestjs/common";
 
 import * as Joi from "joi";
 
 import { WinstonModule } from "nest-winston";
 
 import { InvalidEnvironmentException } from "./exceptions";
+import { MorganMiddleware } from "./middlewares";
 
 @Module({
   imports: [WinstonModule.forRoot({ ...winstonConfiguration })]
 })
-export class CoreModule implements OnModuleInit {
+export class CoreModule implements NestModule, OnModuleInit {
   private validEnvironment(): void {
     const { error }: Joi.ValidationResult<EnvironmentSchema> = environmentSchema.validate(environment);
 
     if (error) {
       throw new InvalidEnvironmentException(error.message);
     }
+  }
+
+  public configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(MorganMiddleware).forRoutes({ method: RequestMethod.ALL, path: "*" });
   }
 
   public onModuleInit(): void {
