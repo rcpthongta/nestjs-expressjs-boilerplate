c44e23235f54c834ca120b0bfb3ad25bb06df078
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-09 17:58:18 +0700

    refactor(logging): use request ID from ClsService if available before generating new one

--- src/configurations/winston/formats/request-id.format.ts
+++ src/configurations/winston/formats/request-id.format.ts
@@ -1,12 +1,13 @@
 import * as winston from "winston";
 
 import { nanoid } from "nanoid";
+import { ClsServiceManager } from "nestjs-cls";
 
 import { TransformableOptions } from "../interfaces";
 
 export const requestId: () => winston.Logform.Format = (): winston.Logform.Format => {
   return winston.format((transformable: TransformableOptions): TransformableOptions => {
-    transformable.rid = nanoid();
+    transformable.rid = ClsServiceManager.getClsService().getId() ?? nanoid();
 
     return transformable;
   })();
