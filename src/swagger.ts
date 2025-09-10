7df2fe8f0a47b740e1f68e545da8aeba1abce576
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-10 12:20:10 +0700

    feat(swagger): enhance Swagger UI configuration and API metadata

---
+++ src/swagger.ts
@@ -0,0 +1,73 @@
+import { environment } from "@environment";
+
+import { Logger } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from "@nestjs/swagger";
+
+import { SwaggerTheme, SwaggerThemeNameEnum } from "swagger-themes";
+
+export class Swagger {
+  private readonly logger: Logger = new Logger(Swagger.name);
+
+  private readonly path: string = "api-docs";
+  private readonly pathUI: string = `${this.path}/ui`;
+  private readonly pathJSON: string = `${this.path}/json`;
+  private readonly pathYAML: string = `${this.path}/yaml`;
+
+  private readonly application: NestExpressApplication;
+
+  public constructor(application: NestExpressApplication) {
+    this.application = application;
+  }
+
+  public run(): void {
+    const builder: DocumentBuilder = new DocumentBuilder();
+    const theme: SwaggerTheme = new SwaggerTheme();
+    const configuration: Omit<OpenAPIObject, "paths"> = builder
+      .setVersion(environment.application.version)
+      .setTitle(environment.application.name)
+      .setDescription("API Documentation")
+      .build();
+    const document: OpenAPIObject = SwaggerModule.createDocument(this.application, configuration, {
+      autoTagControllers: true,
+      deepScanRoutes: true,
+      ignoreGlobalPrefix: false
+    });
+
+    SwaggerModule.setup(this.pathUI, this.application, document, {
+      jsonDocumentUrl: this.pathJSON,
+      yamlDocumentUrl: this.pathYAML,
+      raw: ["json"],
+      swaggerOptions: {
+        deepLinking: true,
+        defaultModelsExpandDepth: -1,
+        displayRequestDuration: true,
+        filter: true,
+        operationsSorter: "method",
+        persistAuthorization: true,
+        showCommonExtensions: true,
+        showExtensions: true,
+        showMutatedRequest: true
+      },
+      customSiteTitle: "API Documentation | OpenAPI (Swagger)",
+      customCss: `
+        ${theme.getBuffer(SwaggerThemeNameEnum.FLATTOP)}
+
+        .parameters-col_name {
+          min-width: 30em;
+          padding: 10px 0px;
+        }
+
+        .parameters-col_description {
+          width: 70em;
+          margin-bottom: 2em;
+        }
+      `,
+      useGlobalPrefix: false
+    });
+
+    this.logger.log(`UI {/${this.pathUI}}:`);
+    this.logger.log(`Json {/${this.pathJSON}}:`);
+    this.logger.log(`Yaml {/${this.pathYAML}}:`);
+  }
+}
