a7a65dd9c89dcb9140a3c702ce8898aa05032bc4
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-12 02:26:39 +0700

    feat(logger): add platform info to system log format

--- src/configurations/winston/formats/system.format.ts
+++ src/configurations/winston/formats/system.format.ts
@@ -9,6 +9,7 @@ export const system: () => winston.Logform.Format = (): winston.Logform.Format =
   return winston.format((transformable: TransformableOptions): TransformableOptions => {
     transformable.hostname = os.hostname();
     transformable.pid = process.pid.toString();
+    transformable.platform = os.platform();
     transformable.node = process.version;
 
     return transformable;
