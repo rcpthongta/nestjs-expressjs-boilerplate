f3595bd5ee4d9288c4a45d0f53788ded384da4da
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-14 16:27:23 +0700

    feat(response-wrapper): integrate i18n translation into response wrapper interceptor

--- src/app/core/modules/response-wrapper/response-wrapper.interceptor.ts
+++ src/app/core/modules/response-wrapper/response-wrapper.interceptor.ts
@@ -1,4 +1,5 @@
-import { WrapperResponseDto, WrapperResponseHeaderDto, WrapperResponseHttpDto } from "@dto";
+import { I18nService } from "@common";
+import { WrapperResponseDto, WrapperResponseStatusDto } from "@dto";
 
 import { CallHandler, ExecutionContext, HttpStatus, Injectable, NestInterceptor } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
@@ -6,13 +7,17 @@ import { Reflector } from "@nestjs/core";
 import { Response } from "express";
 import { map, Observable } from "rxjs";
 
-import { SKIP_RESPONSE_WRAPPER_KEY } from "./response-wrapper.constant";
+import { ResponseMessageOptions } from "./interfaces";
+
+import { RESPONSE_MESSAGE_KEY, SKIP_RESPONSE_WRAPPER_KEY } from "./response-wrapper.constant";
 
 @Injectable()
 export class ResponseWrapperInterceptor<T> implements NestInterceptor<T, T | WrapperResponseDto<T>> {
+  private readonly i18n: I18nService;
   private readonly reflector: Reflector;
 
-  public constructor(reflector: Reflector) {
+  public constructor(i18n: I18nService, reflector: Reflector) {
+    this.i18n = i18n;
     this.reflector = reflector;
   }
 
@@ -26,16 +31,21 @@ export class ResponseWrapperInterceptor<T> implements NestInterceptor<T, T | Wra
     return next.handle().pipe(
       map((data: T): WrapperResponseDto<T> => {
         const response: Response = context.switchToHttp().getResponse();
+        const translate: ResponseMessageOptions = this.reflector.get(RESPONSE_MESSAGE_KEY, context.getHandler()) ?? {
+          key: "response.success.ars000001"
+        };
+        const message: string = this.i18n.translate(translate.key, {
+          args: translate?.args,
+          language: translate?.language
+        });
 
         return new WrapperResponseDto({
           success: true,
-          http: new WrapperResponseHttpDto({
+          status: new WrapperResponseStatusDto({
             code: response.statusCode,
             message: HttpStatus[response.statusCode]
           }),
-          header: new WrapperResponseHeaderDto({
-            timestamp: new Date().toISOString()
-          }),
+          message: message,
           payload: data
         });
       })
