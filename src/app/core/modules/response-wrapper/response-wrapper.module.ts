5a81f1eacb080f2214041fd5932a7bf4fd01f6d3
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-22 17:46:24 +0700

    feat(module): register ResponseWrapperFilter in ResponseWrapperModule

--- src/app/core/modules/response-wrapper/response-wrapper.module.ts
+++ src/app/core/modules/response-wrapper/response-wrapper.module.ts
@@ -1,10 +1,15 @@
 import { Module } from "@nestjs/common";
-import { APP_INTERCEPTOR } from "@nestjs/core";
+import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
 
+import { ResponseWrapperFilter } from "./response-wrapper.filter";
 import { ResponseWrapperInterceptor } from "./response-wrapper.interceptor";
 
 @Module({
   providers: [
+    {
+      provide: APP_FILTER,
+      useClass: ResponseWrapperFilter
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: ResponseWrapperInterceptor
