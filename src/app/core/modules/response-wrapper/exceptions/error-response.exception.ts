1312a753c49f5e6ce7af8a5c27ae167b1ae23dda
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-22 17:42:03 +0700

    feat(exception): add custom ErrorResponseException extending HttpException

---
+++ src/app/core/modules/response-wrapper/exceptions/error-response.exception.ts
@@ -0,0 +1,31 @@
+import { HttpException } from "@nestjs/common";
+
+import {
+  ErrorResponseExceptionOptions,
+  ErrorResponseExceptionResponseMessageOptions as ResponseMessage
+} from "../interfaces";
+
+export class ErrorResponseException extends HttpException {
+  private readonly success: boolean;
+  private readonly responseMessage: ResponseMessage;
+
+  public constructor(response: string | object, statusCode: number, options?: ErrorResponseExceptionOptions) {
+    super(response, statusCode);
+
+    this.name = new.target.name;
+    this.success = options?.success ?? false;
+    this.responseMessage = {
+      key: "response.error.are000001",
+      args: options?.responseMessage?.args,
+      language: options?.responseMessage?.language
+    };
+  }
+
+  public getSuccess(): boolean {
+    return this.success;
+  }
+
+  public getResponseMessage(): ResponseMessage {
+    return this.responseMessage;
+  }
+}
