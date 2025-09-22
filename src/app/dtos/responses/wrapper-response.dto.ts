0087f88aaceba2e3a67b1e905623adc095a8d3a9
Ratchapol Thongta <rcp.thongta@proton.me>
2025-09-22 16:52:37 +0700

    refactor(dto): remove readonly modifiers and add WrapperResponseErrorDto

--- src/app/dtos/responses/wrapper-response.dto.ts
+++ src/app/dtos/responses/wrapper-response.dto.ts
@@ -1,6 +1,8 @@
+import { OmitType } from "@nestjs/swagger";
+
 export class WrapperResponseStatusDto {
-  public readonly code: number;
-  public readonly message: string;
+  public code: number;
+  public message: string;
 
   public constructor(initial: Required<WrapperResponseStatusDto>) {
     this.code = initial.code;
@@ -9,15 +11,28 @@ export class WrapperResponseStatusDto {
 }
 
 export class WrapperResponseDto<T> {
-  public readonly success: boolean;
-  public readonly status: WrapperResponseStatusDto;
-  public readonly message: string;
-  public readonly payload: T;
+  public success: boolean;
+  public status: WrapperResponseStatusDto;
+  public message: string;
+  public payload: T;
 
   public constructor(initial: Required<WrapperResponseDto<T>>) {
     this.success = initial.success;
     this.status = initial.status;
     this.message = initial.message;
     this.payload = initial.payload;
+  }
+}
+
+export class WrapperResponseErrorDto extends OmitType(WrapperResponseDto, ["payload"]) {
+  public error: string | object;
+
+  public constructor(initial: Required<WrapperResponseErrorDto>) {
+    super();
+
+    this.success = initial.success;
+    this.status = initial.status;
+    this.message = initial.message;
+    this.error = initial.error;
   }
 }
