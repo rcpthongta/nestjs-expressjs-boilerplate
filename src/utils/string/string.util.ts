d831d47cb66d09d2c20be8b2727c26f0b4b39c32
Ratchapol Thongta <rcp.thongta@proton.me>
2025-10-01 08:49:52 +0700

    feat(string-util): enhance format method to support transformers and error handling

--- src/utils/string/string.util.ts
+++ src/utils/string/string.util.ts
@@ -1,9 +1,20 @@
 import stringFormat from "string-format";
 
-import { FormatArgs } from "./types";
+import { FormatException } from "./exceptions";
+import { FormatArgs, FormatTransformers } from "./types";
 
 export class StringUtil {
-  public static format(template: string, ...args: FormatArgs): string {
-    return stringFormat(template, ...args);
+  private static shouldApplyTransformers(transformers?: FormatTransformers | null): transformers is FormatTransformers {
+    return transformers !== null && transformers !== undefined && Object.keys(transformers).length > 0;
+  }
+
+  public static format(template: string, args: FormatArgs, transformers?: FormatTransformers | null): string {
+    try {
+      return this.shouldApplyTransformers(transformers)
+        ? stringFormat.create(transformers)(template, ...args)
+        : stringFormat(template, ...args);
+    } catch (error: unknown) {
+      throw new FormatException(error);
+    }
   }
 }
