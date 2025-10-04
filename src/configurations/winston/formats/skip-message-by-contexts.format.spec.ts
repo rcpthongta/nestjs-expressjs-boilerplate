5c08a71972ad63686548dc43e643e88d9f6ce54f
Ratchapol Thongta <rcp.thongta@proton.me>
2025-10-05 01:40:53 +0700

    test(winston): add unit tests for skipMessageByContexts and skipMessageByLevels formatters

---
+++ src/configurations/winston/formats/skip-message-by-contexts.format.spec.ts
@@ -0,0 +1,55 @@
+import * as winston from "winston";
+
+import { skipMessageByContexts } from "./skip-message-by-contexts.format";
+
+describe("skipMessageByContexts (Unit)", (): void => {
+  describe("Success cases", (): void => {
+    it("should skip log message if context is in skip list", (): void => {
+      const format: winston.Logform.Format = skipMessageByContexts({ list: ["Bootstrap"] });
+      const output: boolean | winston.Logform.TransformableInfo = format.transform({
+        level: "info",
+        message: "Test",
+        context: "Bootstrap"
+      });
+
+      expect(output).toBeFalsy();
+    });
+
+    it("should allow log message if context is not in skip list", (): void => {
+      const format: winston.Logform.Format = skipMessageByContexts({ list: [] });
+      const input: winston.Logform.TransformableInfo = {
+        level: "info",
+        message: "Test",
+        context: "Bootstrap"
+      };
+      const output: boolean | winston.Logform.TransformableInfo = format.transform(input);
+
+      expect(output).toEqual(expect.objectContaining(input));
+    });
+
+    it.each([
+      {
+        description: "undefined",
+        context: undefined
+      },
+      {
+        description: "null",
+        context: null
+      },
+      {
+        description: "",
+        context: "empty"
+      }
+    ])("should allow log message if context is $description", ({ context }): void => {
+      const format: winston.Logform.Format = skipMessageByContexts({ list: ["Bootstrap"] });
+      const input: winston.Logform.TransformableInfo = {
+        level: "warn",
+        message: "Something happened",
+        context
+      };
+      const output: boolean | winston.Logform.TransformableInfo = format.transform(input);
+
+      expect(output).toEqual(expect.objectContaining(input));
+    });
+  });
+});
---
+++ src/configurations/winston/formats/skip-message-by-levels.format.spec.ts
@@ -0,0 +1,30 @@
+import * as winston from "winston";
+
+import { skipMessageByLevels } from "./skip-message-by-levels.format";
+
+describe("skipMessageByLevels (Unit)", (): void => {
+  describe("Success cases", (): void => {
+    it("should skip log message if level is in skip list", (): void => {
+      const format: winston.Logform.Format = skipMessageByLevels({ list: ["info"] });
+      const output: boolean | winston.Logform.TransformableInfo = format.transform({
+        level: "info",
+        message: "Test",
+        context: "Bootstrap"
+      });
+
+      expect(output).toBeFalsy();
+    });
+
+    it("should allow log message if level is not in skip list", (): void => {
+      const format: winston.Logform.Format = skipMessageByLevels({ list: [] });
+      const input: winston.Logform.TransformableInfo = {
+        level: "info",
+        message: "Test",
+        context: "Bootstrap"
+      };
+      const output: boolean | winston.Logform.TransformableInfo = format.transform(input);
+
+      expect(output).toEqual(expect.objectContaining(input));
+    });
+  });
+});
