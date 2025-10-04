2d2e78ffb042f540fa4245b0dbacf1cb3117883f
Ratchapol Thongta <rcp.thongta@proton.me>
2025-10-04 07:22:07 +0700

    feat(format): add error and cause properties to TransformableOptions

--- src/configurations/winston/interfaces/transformable-options.interface.ts
+++ src/configurations/winston/interfaces/transformable-options.interface.ts
@@ -1,7 +1,9 @@
 import * as winston from "winston";
 
 export interface TransformableOptions extends winston.Logform.TransformableInfo {
+  cause: unknown;
   context: string;
+  error: Error;
   hostname: string;
   label: string;
   message: string;
