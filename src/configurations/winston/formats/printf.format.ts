f19d30b69505170ef3f7993a4849967bdb26d315
Ratchapol Thongta <rcp.thongta@proton.me>
2025-10-04 07:24:50 +0700

    feat(format): enhance printf formatter with error and cause support

--- src/configurations/winston/formats/printf.format.ts
+++ src/configurations/winston/formats/printf.format.ts
@@ -2,18 +2,48 @@ import * as winston from "winston";
 
 import { TransformableOptions } from "../interfaces";
 
-export const printf: () => winston.Logform.Format = (): winston.Logform.Format => {
+function generateErrorCause(cause: unknown): string {
+  let result: string = "";
+
+  if (cause) {
+    result += "\n[Caused by] ";
+
+    if (cause instanceof Error) {
+      result += cause.stack;
+    } else if (typeof cause === "string" || typeof cause === "boolean" || typeof cause === "number") {
+      result += String(cause);
+    } else if (typeof cause === "object") {
+      result += JSON.stringify(cause);
+    }
+  }
+
+  return result;
+}
+
+export function printf(): winston.Logform.Format {
   return winston.format.printf((transformable: TransformableOptions): string => {
     let stack: string = "";
 
-    if (transformable.stack) {
+    if (transformable.error) {
+      stack = `\n${transformable.error.stack}`;
+
+      const cause: string = generateErrorCause(transformable.error.cause);
+
+      if (cause !== "") stack += cause;
+    }
+
+    if (stack === "" && transformable.stack) {
       if (typeof transformable.stack === "string" && transformable.stack.trim()) {
         stack = `\n${transformable.stack}`;
       } else if (Array.isArray(transformable.stack) && transformable.stack.length > 0) {
         stack = `\n${transformable.stack.filter((message: string): boolean => message !== undefined).join("\n")}`;
       }
+
+      const cause: string = generateErrorCause(transformable.cause);
+
+      if (cause !== "") stack += cause;
     }
 
     return `${transformable.label} [${transformable.pid} • ${transformable.hostname} (${transformable.platform}) • Node.js ${transformable.node}] ${transformable.rid} - ${transformable.timestamp}   ${transformable.level} [${transformable.context}] ${transformable.message} ${transformable.ms}${stack}`;
   });
-};
+}
