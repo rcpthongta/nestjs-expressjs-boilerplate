895576af57cbea6e099012c74d72b9e94bac6d77
Ratchapol Thongta <rcp.thongta@proton.me>
2025-10-04 07:13:20 +0700

    refactor(format): simplify skipMessage formatters with ternary expressions

--- src/configurations/winston/formats/skip-message-by-contexts.format.ts
+++ src/configurations/winston/formats/skip-message-by-contexts.format.ts
@@ -3,11 +3,7 @@ import * as winston from "winston";
 import { SkipMessageOptions, TransformableOptions } from "../interfaces";
 
 export function skipMessageByContexts(opts: SkipMessageOptions): winston.Logform.Format {
-  return winston.format((transformable: TransformableOptions): boolean | TransformableOptions => {
-    if (opts.list.includes(transformable.context)) {
-      return false;
-    }
-
-    return transformable;
-  })();
+  return winston.format((transformable: TransformableOptions): boolean | TransformableOptions =>
+    opts.list.includes(transformable.context) ? false : transformable
+  )();
 }
--- src/configurations/winston/formats/skip-message-by-levels.format.ts
+++ src/configurations/winston/formats/skip-message-by-levels.format.ts
@@ -3,11 +3,7 @@ import * as winston from "winston";
 import { SkipMessageOptions, TransformableOptions } from "../interfaces";
 
 export function skipMessageByLevels(opts: SkipMessageOptions): winston.Logform.Format {
-  return winston.format((transformable: TransformableOptions): boolean | TransformableOptions => {
-    if (opts.list.includes(transformable.level.toLowerCase().trim())) {
-      return false;
-    }
-
-    return transformable;
-  })();
+  return winston.format((transformable: TransformableOptions): boolean | TransformableOptions =>
+    opts.list.includes(transformable.level.toLowerCase().trim()) ? false : transformable
+  )();
 }
