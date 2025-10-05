71f0487ffdfc165457bf1fe235a403aadf22c8b6
Ratchapol Thongta <rcp.thongta@proton.me>
2025-10-05 23:03:31 +0700

    test(winston): refactor skipMessageByLevels test cases for clarity and scalability

--- src/configurations/winston/formats/skip-message-by-levels.format.spec.ts
+++ src/configurations/winston/formats/skip-message-by-levels.format.spec.ts
@@ -15,8 +15,17 @@ describe("skipMessageByLevels (Unit)", (): void => {
       expect(output).toBeFalsy();
     });
 
-    it("should allow log message if level is not in skip list", (): void => {
-      const format: winston.Logform.Format = skipMessageByLevels({ list: [] });
+    it.each([
+      {
+        name: "should allow log message if level does not match any item in skip list",
+        list: ["debug"]
+      },
+      {
+        name: "should allow log message if level is not in skip list",
+        list: []
+      }
+    ])("$name", ({ list }): void => {
+      const format: winston.Logform.Format = skipMessageByLevels({ list });
       const input: winston.Logform.TransformableInfo = {
         level: "info",
         message: "Test",
